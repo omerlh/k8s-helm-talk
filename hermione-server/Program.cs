362bd907b61ab3464ea4851c345ff931d69a455d
Omer Levi Hevroni <omerlh@gmail.com>
2018-09-20 14:58:53 +0300

    fix hermione

--- hermione-server/Program.cs
+++ hermione-server/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AmazingApi
+namespace HermioneServer
 {
     public class Program
     {
